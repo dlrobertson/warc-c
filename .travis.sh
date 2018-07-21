25795861bec3500e706a8022c66b99a89cad2988
Dan Robertson <dan@dlrobertson.com>
2018-07-21 01:26:10 +0000

    Add travis config

---
+++ .travis.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z "${GEN}" -o -z "${BUILD_TYPE}" ]
+then
+    echo "Must define GEN and BUILD_TYPE"
+    exit 1
+fi
+
+mkdir ./build
+cd build
+
+cmake -G "${GEN}" -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DBUILD_TESTS=ON ..
+
+if [[ "${GEN}" == "Ninja" ]]
+then
+    ninja -v
+else
+    VERBOSE=1 make
+fi
+
+./tests/warc-c_test
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # `warc-c`
 
-C Library for parsing [WARC] files.
+A WIP C Library for parsing [WARC] files.
 
 [WARC]: https://iipc.github.io/warc-specifications/specifications/warc-format/warc-1.1/
